4fcb9a9249cf2ca44902b424c40039bf1c7d15e3
skyhjune <skyhjune@naver.com>
2026-08-07 02:51:20 +0900

    ✨ Feat. 컨셉 목록 로딩 스켈레톤 적용
    Why: isLoading 분기가 없어 컨셉 응답 전까지 상단바만 남고
         본문이 통째로 비어 있음
    
    How: CardStudioDetail과 같은 치수(230/98/90)의 카드 스켈레톤 2장을
         섹션 제목 자리와 함께 노출. 조건부로 렌더되는 옵션 문구 줄은
         제외해 옵션 없는 카드 높이(419px)에 맞춤
    
    Tag: #feat #concept-list #skeleton
    See: #88

--- src/pages/studio/ConceptListPage.tsx
+++ src/pages/studio/ConceptListPage.tsx
@@ -7,6 +7,7 @@ import Toast from '@/components/common/Toast'
 import { IcError } from '@/components/icons'
 import NavigationBar from '@/components/layout/NavigationBar'
 
+import ConceptListSkeleton from '@/pages/studio/components/ConceptListSkeleton'
 import DateChangeSheet from '@/pages/studio/components/DateChangeSheet'
 import ErrorNotice from '@/pages/studio/components/ErrorNotice'
 import { getShootingCategoryLabel } from '@/constants/shootingCategory'
@@ -82,6 +83,7 @@ const ConceptListPage = () => {
   // 시간대가 정해지면 응답에 selectedSlot(예약 가능 여부)이 실리도록 재조회한다.
   const {
     data: products,
+    isLoading: isProductsLoading,
     isError: productsError,
     refetch: refetchProducts,
   } = useStudioProducts(
@@ -244,6 +246,8 @@ const ConceptListPage = () => {
             onRetry={() => refetchProducts()}
           />
         </div>
+      ) : isProductsLoading ? (
+        <ConceptListSkeleton />
       ) : (
         <main className="flex flex-col px-5 pb-6">
           {products?.productGroups.map((group) => (
